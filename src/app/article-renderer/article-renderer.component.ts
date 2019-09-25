9f312ce7d3785a8d202707d0439b48b9a85f6f9a
tokhichevsky <lerak01@yandex.ru>
2019-09-25 21:05:35 +0300

    corrected styles.less

--- src/app/article-renderer/article-renderer.component.ts
+++ src/app/article-renderer/article-renderer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IArticle } from '../interfaces/IArticle';
 
 @Component({
   selector: 'app-article-renderer',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-renderer.component.less']
 })
 export class ArticleRendererComponent implements OnInit {
-
+  @Input() article: IArticle;
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
